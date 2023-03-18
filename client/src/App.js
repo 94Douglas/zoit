26dc286f5b442f652760a3f4d87f95a86ada3e58
Douglas S <94dousch@gafe.molndal.se>
2023-03-18 12:54:38 +0100

    Fixed animated background

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Router>
         <div>
           <NavigationBar />
+
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,24 +2,35 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="container">
-      <div className="card-wrapper">
-        <h2 className="noFont">
-          Är du som privatperson, företag eller föreningen på jakt efter någon
-          som kan skapa en hemsida för ett sjyst pris?!
-        </h2>
-        <h3 className="noFont">
-          Jag gör allt från lätta hemsidor till mer avancerade. Vi börjar med
-          ett första möte där vi går igenom era önskemål. <br />
-          Det är viktigt för mig att mina kunder känner sig trygga med mig som
-          konsult samt att dom får det dem önskar. <br />
-          Pris varierar beroende på vad man vill ha för funktionalitet. Men
-          eftersom jag är i början på min karriär kan jag garantera ett
-          billigare pris än vanliga byråer. <br />
-          Min karriär har precis startat men det här är bara början. Tveka inte
-          att höra av er så spånar vi vidare!
-        </h3>
+    <div>
+      <div className="container">
+        <div className="card-wrapper">
+          <p id="head1" className="header">
+            Är du som privatperson, företag eller föreningen på jakt efter någon
+            som kan skapa en hemsida för ett sjyst pris?!
+          </p>
+          <p id="head2" className="header">
+            Jag gör allt från lätta hemsidor till mer avancerade. Vi börjar med
+            ett första möte där vi går igenom era önskemål. <br />
+            Det är viktigt för mig att mina kunder känner sig trygga med mig som
+            konsult samt att dom får det dem önskar. <br />
+            Pris varierar beroende på vad man vill ha för funktionalitet. Men
+            eftersom jag är i början på min karriär kan jag garantera ett
+            billigare pris än vanliga byråer. <br />
+            Min karriär har precis startat men det här är bara början. Tveka
+            inte att höra av er så spånar vi vidare!
+          </p>
+        </div>
       </div>
+      <div className="light x1"></div>
+      <div className="light x2"></div>
+      <div className="light x3"></div>
+      <div className="light x4"></div>
+      <div className="light x5"></div>
+      <div className="light x6"></div>
+      <div className="light x7"></div>
+      <div className="light x8"></div>
+      <div className="light x9"></div>
     </div>
   );
 };
