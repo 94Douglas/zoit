0249dfd52e2dcd525020ec5ebf94ed83f8fc4a50
Douglas S <94dousch@gafe.molndal.se>
2023-04-09 12:23:29 +0200

    Fixed some design issues

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -3,62 +3,62 @@ import MissingImg from "../assets/noimage.png";
 
 const Portfolio = () => {
   return (
-    <div className="row container">
-      <div className="col-sm-8 mb-3 mb-sm-0">
-        <div className="card h-100 hovering-card">
-          <img
-            src={MissingImg}
-            style={{
-              height: "70px",
-              width: "70px",
-              margin: "0px auto",
-            }}
-            alt=""
-          />
-          <div className="card-body">
-            <h5 className="card-title mb-4">Olovs Hage Samfällighet</h5>
-            <p className="card-text mb-5">
-              En samfällighet var i behov av en hemsida de kunde lägga ut
-              uppgifter till styrelsen och årsredovisning
-            </p>
-            <a
-              href="https://olovshage.se/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Till Hemsidan
-            </a>
-          </div>
+    <div className="row container-portfolio">
+      {/* <div className="col-sm-8 mb-3 mb-sm-0"> */}
+      <div className="card h-100 hovering-card">
+        <img
+          src={MissingImg}
+          style={{
+            height: "70px",
+            width: "70px",
+            margin: "0px auto",
+          }}
+          alt=""
+        />
+        <div className="card-body">
+          <h5 className="card-title mb-4">Olovs Hage Samfällighet</h5>
+          <p className="card-text mb-5">
+            En samfällighet var i behov av en hemsida de kunde lägga ut
+            uppgifter till styrelsen och årsredovisning
+          </p>
+          <a
+            href="https://olovshage.se/"
+            className="btn btn-primary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Till Hemsidan
+          </a>
         </div>
+        {/* </div> */}
       </div>
-      <div className="col-md-4">
-        <div className="card h-100 hovering-card">
-          <img
-            src={MissingImg}
-            style={{
-              height: "70px",
-              width: "70px",
-              margin: "0px auto",
-            }}
-            alt=""
-          />
-          <div className="card-body h-100">
-            <h5 className="card-title mb-4">BRF Hällungen 2</h5>
-            <p className="card-text mb-5">
-              Var i behov av en hemsida där de kunde ladda upp bland annat
-              årsredovisning
-            </p>
-            <a
-              href="https://brfhallungen.se/"
-              className="btn btn-primary mt-4"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Till Hemsidan
-            </a>
-          </div>
+      {/* <div className="col-md-4"> */}
+      <div className="card h-100 hovering-card">
+        <img
+          src={MissingImg}
+          style={{
+            height: "70px",
+            width: "70px",
+            margin: "0px auto",
+          }}
+          alt=""
+        />
+        <div className="card-body h-100">
+          <h5 className="card-title mb-4">BRF Hällungen 2</h5>
+          <p className="card-text mb-5">
+            Var i behov av en hemsida där de kunde ladda upp bland annat
+            årsredovisning
+          </p>
+          <a
+            href="https://brfhallungen.se/"
+            className="btn btn-primary mt-4"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Till Hemsidan
+          </a>
         </div>
+        {/* </div> */}
       </div>
     </div>
   );
