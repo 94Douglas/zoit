6d716618761629321d8618fcb127ccd328e79f5b
Douglas <douglas.schoug@gmail.com>
2023-07-03 15:58:32 +0200

    UI fixes to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,28 +1,32 @@
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import React from "react";
-import { AiOutlineCopyrightCircle } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const linkedin =
+    "https://www.linkedin.com/company/zeroone-it-web/?viewAsMember=true";
   const year = new Date().getFullYear();
+
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <footer className="footer">
       <div className="footer-section">
-        <img src="logo-url" alt="Company Logo" className="footer-logo" />
-        <p>
-          Your Company's mission statement or description goes here. This helps
-          reinforce your brand and provides information to visitors.
+        <h3>ZeroOne IT</h3>
+        <p className="footer-color">
+          Vi utvecklar din hemsida på professionell nivå med noggrannhet.
         </p>
       </div>
       <div className="footer-section">
-        <h3>Services</h3>
+        <h3>Tjänster</h3>
         <ul className="footer-list">
           <li>
             <a href="#design">Design</a>
           </li>
           <li>
-            <a href="#development">Development</a>
-          </li>
-          <li>
-            <a href="#marketing">Marketing</a>
+            <a href="#development">Utveckling</a>
           </li>
         </ul>
       </div>
@@ -30,53 +34,35 @@ const Footer = () => {
         <h3>Quick Links</h3>
         <ul className="footer-list">
           <li>
-            <a href="#about">About Us</a>
+            <a href="/about">Om Oss</a>
           </li>
           <li>
-            <a href="#careers">Careers</a>
+            <a href="/contact">Kontakta Oss</a>
           </li>
           <li>
-            <a href="#contact">Contact Us</a>
+            <a href="/portfolio">Portfolio</a>
           </li>
         </ul>
       </div>
       <div className="footer-section">
-        <h3>Connect with Us</h3>
+        <h3>Connecta Med Oss</h3>
         <ul className="social-list">
           <li>
-            <a href="#">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="fab fa-instagram"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="fab fa-linkedin"></i>
+            <a
+              style={{ cursor: "pointer" }}
+              onClick={() => openInNewTab(linkedin)}
+            >
+              <LinkedInIcon /> Hitta oss på LinkedIn
             </a>
           </li>
         </ul>
       </div>
+
       <div className="footer-section">
-        <h3>Newsletter</h3>
-        <form>
-          <input type="email" placeholder="Email address" required />
-          <button type="submit">Subscribe</button>
-        </form>
-      </div>
-      <div className="footer-section">
-        <h3>Contact Info</h3>
-        <p>123 Your Address, Your City, Your Country</p>
-        <p>+1234567890</p>
-        <p>email@yourcompany.com</p>
+        <h3>Kontakt Information</h3>
+        <p className="footer-color">
+          Tryck <Link to="/contact">HÄR</Link> för att kontakta oss!
+        </p>
       </div>
     </footer>
   );
--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,4 +1,3 @@
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -7,15 +6,12 @@ import Typography from "@mui/material/Typography";
 import * as React from "react";
 
 const Portfolio = () => {
-  const bull = (
-    <Box
-      component="span"
-      sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-    >
-      •
-    </Box>
-  );
+  const olovsHage = "https://olovshage.se/";
+  const recruitZone = "https://94douglas.github.io/workers/";
 
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
   return (
     <>
       <div className="container">
@@ -33,35 +29,102 @@ const Portfolio = () => {
               color="white"
               gutterBottom
             >
-              Word of the Day
+              Bostadsrättförening
             </Typography>
             <Typography
               variant="h5"
               component="div"
               color="white"
-              sx={{ fontFamily: "Helvetica" }}
+              sx={{
+                marginBottom: 1.5,
+                fontFamily: "Helvetica",
+                textDecoration: "underline",
+              }}
             >
-              be{bull}nev{bull}o{bull}lent
+              BRF Hällungen 2
+            </Typography>
+
+            <Typography
+              variant="body2"
+              color="white"
+              sx={{ marginBottom: 1.5, fontFamily: "Helvetica" }}
+            >
+              Tillsammans med styrelsen utförde vi en hemsida helt från scratch.
+              Syftet med deras hemsida skulle bli att det skulle finnas
+              information kring föreningen för nyinflyttade eller för de
+              personer som funderar på att köpa en bostad.
+              <br />
+              På hemsidan kan man läsa årsredovisning.
+              <br />
+              Kontaktuppgifter till styrelsen.
+              <br />
+              Välkomstinformation för de som är intresserade av en bostad eller
+              är nyinflyttade.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" sx={{ fontFamily: "Helvetica" }}>
+              Besök
+            </Button>
+          </CardActions>
+        </Card>
+        <Card
+          sx={{
+            minWidth: 275,
+            background: "rgba(25, 25, 28)",
+            fontFamily: "Helvetica",
+            marginBottom: "15px",
+          }}
+        >
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 14, fontFamily: "Helvetica" }}
+              color="white"
+              gutterBottom
+            >
+              Samfällighet
             </Typography>
             <Typography
-              sx={{ mb: 1.5, color: "white", fontFamily: "Helvetica" }}
+              variant="h5"
+              component="div"
+              color="white"
+              sx={{
+                mb: 1.5,
+                fontFamily: "Helvetica",
+                textDecoration: "underline",
+              }}
             >
-              adjective
+              Samfällighet Olovs Hage
             </Typography>
+
             <Typography
               variant="body2"
               color="white"
               sx={{ fontFamily: "Helvetica" }}
             >
-              well meaning and kindly.
+              Tillsammans med styrelsen utförde vi en hemsida helt från scratch.
+              Syftet med deras hemsida skulle bli att det skulle finnas
+              information kring samfälligheten för nyinflyttade eller för de
+              personer som funderar på att köpa en bostad.
+              <br />
+              På hemsidan kan man läsa årsredovisning.
+              <br />
+              Kontaktuppgifter till styrelsen.
+              <br />
+              Information om styrelsen och området.
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small" sx={{ fontFamily: "Helvetica" }}>
-              Learn More
+            <Button
+              size="small"
+              sx={{ fontFamily: "Helvetica" }}
+              onClick={() => openInNewTab(olovsHage)}
+            >
+              Besök
             </Button>
           </CardActions>
         </Card>
+
         <Card
           sx={{
             minWidth: 275,
@@ -75,95 +138,44 @@ const Portfolio = () => {
               color="white"
               gutterBottom
             >
-              Word of the Day
+              Intern Projekt
             </Typography>
             <Typography
               variant="h5"
               component="div"
               color="white"
-              sx={{ fontFamily: "Helvetica" }}
+              sx={{
+                mb: 1.5,
+                fontFamily: "Helvetica",
+                textDecoration: "underline",
+              }}
             >
-              be{bull}nev{bull}o{bull}lent
+              Recruit Zone
             </Typography>
-            <Typography
-              sx={{ mb: 1.5, color: "white", fontFamily: "Helvetica" }}
-            >
-              adjective
-            </Typography>
+
             <Typography
               variant="body2"
               color="white"
               sx={{ fontFamily: "Helvetica" }}
             >
-              well meaning and kindly.
+              Hemsidan är under uppbyggnad, men finns tillgänglig för
+              allmänheten.
+              <br />
+              Tryck på knappen nedan för att utforska!
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small" sx={{ fontFamily: "Helvetica" }}>
-              Learn More
+            <Button
+              size="small"
+              sx={{ fontFamily: "Helvetica" }}
+              onClick={() => openInNewTab(recruitZone)}
+            >
+              Besök
             </Button>
           </CardActions>
         </Card>
       </div>
     </>
-    // <div className="row container-portfolio">
-    //   {/* <div className="col-sm-8 mb-3 mb-sm-0"> */}
-    //   <div className="card h-100 hovering-card">
-    //     <img
-    //       src={MissingImg}
-    //       style={{
-    //         height: "70px",
-    //         width: "70px",
-    //         margin: "0px auto",
-    //       }}
-    //       alt=""
-    //     />
-    //     <div className="card-body">
-    //       <h5 className="card-title mb-4">Olovs Hage Samfällighet</h5>
-    //       <p className="card-text mb-5">
-    //         En samfällighet var i behov av en hemsida de kunde lägga ut
-    //         uppgifter till styrelsen och årsredovisning
-    //       </p>
-    //       <a
-    //         href="https://olovshage.se/"
-    //         className="btn btn-primary"
-    //         target="_blank"
-    //         rel="noreferrer"
-    //       >
-    //         Till Hemsidan
-    //       </a>
-    //     </div>
-    //     {/* </div> */}
-    //   </div>
-    //   {/* <div className="col-md-4"> */}
-    //   <div className="card h-100 hovering-card">
-    //     <img
-    //       src={MissingImg}
-    //       style={{
-    //         height: "70px",
-    //         width: "70px",
-    //         margin: "0px auto",
-    //       }}
-    //       alt=""
-    //     />
-    //     <div className="card-body h-100">
-    //       <h5 className="card-title mb-4">BRF Hällungen 2</h5>
-    //       <p className="card-text mb-5">
-    //         Var i behov av en hemsida där de kunde ladda upp bland annat
-    //         årsredovisning
-    //       </p>
-    //       <a
-    //         href="https://brfhallungen.se/"
-    //         className="btn btn-primary mt-4"
-    //         target="_blank"
-    //         rel="noreferrer"
-    //       >
-    //         Till Hemsidan
-    //       </a>
-    //     </div>
-    //     {/* </div> */}
-    //   </div>
-    // </div>
   );
 };
 
